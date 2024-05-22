d7b7a49eb728384fb54e2cb7c8a1fd9dc34b16e7
gasimmons <146766169+gasimmons@users.noreply.github.com>
2024-05-21 23:07:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ files = ["../public/lab4/Chow_Rep1.genes.results",
          "../public/lab4/HFD_Rep2.genes.results",
          "../public/lab4/HFD_Rep3.genes.results"]
 
-# This might not be necessary, had it in my lab though...
 conditions = ["Chow"] * 3 + ["HFD"] * 3
 
 samples = pd.DataFrame({"run": [f.split("/")[-1] for f in files],
@@ -22,20 +21,20 @@ df_final = pd.DataFrame()
 # Loop over each file
 for i, file in enumerate(files):
     # Read the file into a DataFrame
-    df = pd.read_csv(file, sep="\t", usecols=[0, 5], names=["gene_id", f"tpm_{i+1}"], header=None)
-    
+    df = pd.read_csv(file, sep="\t", usecols=[0, 4], names=["gene_id", samples.iloc[i, 0]], header=None)
+
     # If this is the first file, copy it to df_final
     if df_final.empty:
         df_final = df
     else:
         # Otherwise, merge the new DataFrame with df_final
-        df_final = pd.merge(df_final, df, on="gene_id")
+        df_final = pd.merge(df_final, df, on="gene_id", how="outer")
 
 # Write the final DataFrame to a CSV file
 df_final.to_csv("merged_file.csv", index=False)
 
-#Do I need to set lengths to 0, this was in read me:
-# 2. Note, we also used txi$length[txi$length == 0] <- 1 
+# I believe this has the same data as the dds from the lab, just as a csv, not sure what txi is necessary for, if someone could figure that part out.
 
+# Have to include filtering out low counts
 
-#This gets a file in the same format as the RSEM file needed to run Deseq
+#Final part is actually implementing deseq, need to get out "gene_id""baseMean","log2FoldChange","lfcSE","stat","pvalue","padj" from the dds
