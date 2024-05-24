b879127c11fa6557b82754e49d0992883e1ac221
Gayathri Donepudi <136131186+gayathri-donepudi@users.noreply.github.com>
2024-05-24 11:03:47 -0700

    Update simpletest.py

--- simpletest.py
+++ simpletest.py
@@ -4,6 +4,7 @@ from dataset import DESeqDataSet
 from normalization import size_factors, normalize_counts
 from dispersion import estimate_dispersions, dispersion_trend, fit_dispersion_trend, shrink_dispersions
 from differential_expression import deseq
+from utilities import plotVolcano, plotHeatmap
 
 def main():
     # Sample data
