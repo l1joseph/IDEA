b34e133171fe8aa424f507f731a087213fceeaa8
Gayathri Donepudi <136131186+gayathri-donepudi@users.noreply.github.com>
2024-05-24 16:26:49 -0700

    Update __init__.py

--- idea/__init__.py
+++ idea/__init__.py
@@ -1,5 +1,5 @@
 from .dataset import IDEADataSet
-from .differential_expression import IDEAResults, deseq, adjust_pvalues
+from .differential_expression import IDEAResults, runidea, adjust_pvalues
 from .dispersion import estimate_dispersions, fit_dispersion_trend, shrink_dispersions
 from .normalization import median_of_ratios, size_factors, normalize_counts
 from .plots import plotVolcano, plotHeatmap
@@ -8,7 +8,7 @@ __version__ = '0.1.0'
 __all__ = [
     'IDEADataSet',
     'IDEAResults',
-    'deseq',
+    'runidea',
     'adjust_pvalues',
     'estimate_dispersions',
     'fit_dispersion_trend',
