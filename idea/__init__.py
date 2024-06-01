9e3ad193bee9bd246eea393e0463e7f8f5a91e57
Gayathri Donepudi <136131186+gayathri-donepudi@users.noreply.github.com>
2024-06-01 16:44:14 -0700

    Update __init__.py

--- idea/__init__.py
+++ idea/__init__.py
@@ -1,6 +1,6 @@
 from .dataset import IDEADataSet
 from .differential_expression import IDEAResults, create_design_matrix, fit_negative_binomial_model, perform_wald_test, compute_log2_fold_change, runidea
-from .dispersion import estimate_dispersions, fit_dispersion_trend, shrink_dispersions
+from .dispersion import estimate_dispersions, fit_dispersion_trend, shrink_dispersions, dispersion_trend
 from .normalization import median_of_ratios, size_factors, normalize_counts
 from .plots import map_ensembl_to_gene_name, plot_volcano, plot_heatmap
 
@@ -14,6 +14,7 @@ __all__ = [
     'compute_log2_fold_change', 
     'runidea',
     'estimate_dispersions',
+    'dispersion_trend',
     'fit_dispersion_trend',
     'shrink_dispersions',
     'median_of_ratios',
